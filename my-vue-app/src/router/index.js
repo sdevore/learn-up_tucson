f1804b4bd2131fe723e5a29d75e76d00ed2e85a5
Sam DeVore <sdevore@gmail.com>
2018-04-19 20:18:26 -0700

    the class day one

---
+++ my-vue-app/src/router/index.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Hello from '@/components/Hello'
+import PostsManager from '@/components/PostsManager'
+import Auth from '@okta/okta-vue'
+
+Vue.use(Auth, {
+  issuer: 'https://dev-986819-admin.oktapreview.com/oauth2/default',
+  client_id: '0oaerx0ga8H2Ar5fa0h7',
+  redirect_uri: 'http://localhost:8080/implicit/callback',
+  scope: 'openid profile email'
+})
+
+Vue.use(Router)
+
+let router = new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'Hello',
+      component: Hello
+    },
+    {
+      path: '/implicit/callback',
+      component: Auth.handleCallback()
+    },
+    {
+      path: '/posts-manager',
+      name: 'PostsManager',
+      component: PostsManager,
+      meta: {
+        requiresAuth: true
+      }
+    }
+  ]
+})
+
+router.beforeEach(Vue.prototype.$auth.authRedirectGuard())
+
+export default router
